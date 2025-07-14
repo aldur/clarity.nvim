1bb9be40fb8aebc0df80ed8e2e38d0a6a6e5a57f
Adriano Di Luzio <aldur@users.noreply.github.com>
2025-07-14 15:39:43 +0200

    Update LSP config

--- lua/clarinet.lua
+++ lua/clarinet.lua
@@ -1,18 +1,11 @@
 local lspconfig_configs = require 'lspconfig.configs'
-local lspconfig_util = require 'lspconfig.util'
 
 if not lspconfig_configs.clarinet then
     lspconfig_configs.clarinet = {
         default_config = {
-            cmd = {'clarinet', 'lsp'},
-            filetypes = {'clarity'},
-            root_dir = lspconfig_util.find_git_ancestor,
-            single_file_support = true,
-            init_options = {}
+            cmd = { 'clarinet', 'lsp' },
+            filetypes = { 'clar', 'clarity' },
+            root_markers = { '.git' },
         },
-        docs = {
-            description = [[https://github.com/hirosystems/clarinet]],
-            default_config = {root_dir = [[root_pattern(".git")]]}
-        }
     }
 end
