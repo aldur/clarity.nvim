727318ce294f96713a2d093d253e0704d729be74
aldur <aldur@users.noreply.github.com>
2025-10-05 20:37:04 +0200

    wip: Switch to `nvim-treesitter` integration

--- plugin/clarity.lua
+++ plugin/clarity.lua
@@ -1,16 +1,14 @@
-vim.filetype.add({extension = {clar = 'clarity'}})
+vim.filetype.add({ extension = { clar = "clarity" } })
 
 -- NOTE: This fails if `nvim-treesitter` is not installed
-local parser_config = require"nvim-treesitter.parsers".get_parser_configs()
-parser_config.clarity = {
-    install_info = {
-        url = "https://github.com/xlittlerag/tree-sitter-clarity",
-        files = {"src/parser.c"},
-        branch = "main",
-        generate_requires_npm = false,
-        requires_generate_from_grammar = false
-    },
-    filetype = "clarity"
+require("nvim-treesitter.parsers").clarity = {
+	install_info = {
+		url = "https://github.com/xlittlerag/tree-sitter-clarity",
+		revision = "ef0552d593a64d6d2936090a44f0ad9f5d54a37f",
+		branch = "main",
+	},
+	maintainers = { "aldur" },
+	tier = 2,
 }
 
-vim.treesitter.language.register('clarity', 'clarity')
+vim.treesitter.language.register("clarity", "clarity")
