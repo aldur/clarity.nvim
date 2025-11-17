6933008ec46bb54c64d98882cbff29a4596c2ed7
adriano-stacks <adriano-stacks@users.noreply.github.com>
2025-11-17 15:58:50 +0100

    Set commentstring

--- ftplugin/clarity.lua
+++ ftplugin/clarity.lua
@@ -1,2 +1,4 @@
 -- Source https://github.com/alexkeating/vim-clarity/blob/main/syntax/clarity.vim
-vim.opt_local.iskeyword = '38,42,43,45,47-58,60-63,64-90,97-122,_'
+vim.opt_local.iskeyword = "38,42,43,45,47-58,60-63,64-90,97-122,_"
+
+vim.opt_local.commentstring = ";; %s"
