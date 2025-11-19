9239c787d964c9946476318a3320096d67804baf
adriano-stacks <adriano-stacks@users.noreply.github.com>
2025-11-19 16:48:37 +0100

    Start treesitter

--- ftplugin/clarity.lua
+++ ftplugin/clarity.lua
@@ -2,3 +2,7 @@
 vim.opt_local.iskeyword = "38,42,43,45,47-58,60-63,64-90,97-122,_"
 
 vim.opt_local.commentstring = ";; %s"
+
+-- ts needs to be started:
+-- https://github.com/nvim-treesitter/nvim-treesitter/blob/bb83a676128d95c865e40ba71376d883bdadec14/README.md#highlighting
+vim.treesitter.start()
